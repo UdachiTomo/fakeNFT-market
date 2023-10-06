111c8eb16caefcc1cfec8b42d05fc38e47a99296
udachi tomo <fragnet1ck@gmail.com>
2023-10-06 18:10:11 +0300

    13.1 исправлен баг с отображением tabBarController после завершения покупки

--- FakeNFT/Scenes /Cart/PurchaseResult/PurchaseResultViewController.swift
+++ FakeNFT/Scenes /Cart/PurchaseResult/PurchaseResultViewController.swift
@@ -67,9 +67,9 @@ final class PurchaseResultViewController: UIViewController {
     }
 
     @objc private func didTapButton() {
-        guard let firstScene = UIApplication.shared.connectedScenes.first as? UIWindowScene else { return }
-        guard let firstWindow = firstScene.windows.first else { return }
-        let window = firstWindow
-        window.rootViewController = CatalogViewController()
+        if let vc = presentingViewController?.presentingViewController as? UITabBarController {
+            vc.selectedIndex = 1
+        }
+        presentingViewController?.presentingViewController?.dismiss(animated: true)
     }
 }
